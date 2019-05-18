f216b176c8b6494b347d53957e2aff5ebf4b39e7
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-05-18 09:01:56 +0530

    build resp

--- src/app/product/add-product/product.model.ts
+++ src/app/product/add-product/product.model.ts
@@ -9,10 +9,6 @@ export class Product {
     price: Number;
     color: string;
     styleCode: string;
-<<<<<<< HEAD
-=======
-    skuCode: string;
->>>>>>> e3a0ea67d1a5f738a38e1638bb7476413ad5f053
     skuCodeVerify: boolean;
     bulletPoints: string;
     productImageName: [string];
@@ -22,12 +18,6 @@ export class Product {
     size: [Size];
     spPrice: String;
     mrpPrice: String;
-<<<<<<< HEAD
-=======
-    moq: Number;
-    /* mfdQty: Number; */
-    mfdQtyEditing: Boolean;
->>>>>>> e3a0ea67d1a5f738a38e1638bb7476413ad5f053
     // details
     material: string;
     weight: string;
